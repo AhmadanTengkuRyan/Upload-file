d28838a451c9e8bf18c903ac58e2818f13859e90
AhmadanTengkuRyan <71076211+AhmadanTengkuRyan@users.noreply.github.com>
2020-09-10 19:05:13 +0700

    Add files via upload

---
+++ UTS.java
@@ -0,0 +1,17 @@
+package UTS_September_2020;
+
+public class UTS {
+
+  public static void main(String[] args) {
+        //UTS September 2020 (Ahmadan Tengku Ryan)
+        String huruf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-";
+		
+		char dy [] =  {huruf.charAt(0),huruf.charAt(7),huruf.charAt(12),huruf.charAt(0),huruf.charAt(3),huruf.charAt(0),huruf.charAt(13),huruf.charAt(26),huruf.charAt(19),huruf.charAt(4),huruf.charAt(13),huruf.charAt(6),huruf.charAt(10),huruf.charAt(20),huruf.charAt(26),huruf.charAt(17),huruf.charAt(24),huruf.charAt(0),huruf.charAt(13)};
+		
+		String nama = new String(dy);
+		
+		System.out.println(nama);
+		
+		//cuma bisa segini ya pak hehe,maaf kalo kurang rapi code nya soalnya pake mobile text editornya
+  }
+}
